5a9a2150eb8d239bbcc677ac22a9cde370825dc7
Melindah11 <melindahmelmel@gmail.com>
2019-09-24 00:04:16 -0700

    perubahan di welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Saya Rubah di sini
                 </div>
 
                 <div class="links">
