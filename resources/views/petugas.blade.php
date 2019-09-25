213ec438ab043c322a9b49fc2577df75b1289479
Your Name <you@example.com>
2019-09-25 15:20:40 +0700

    petugas

--- resources/views/petugas.blade.php
+++ resources/views/petugas.blade.php
@@ -2,53 +2,102 @@
 <html>
 <head>
 	<title>Tutorial Membuat CRUD </title>
-</head>
-<body>
- 
-	
-	<h3>Data Petugas</h3>
- 
-	<a href="/petugas/tambah"> + Tambah Petugas Baru</a>
-	
-	<br/>
-	<br/>
- 
-	<table border="1">
-		<tr>
-			<th>id </th>
-			<th>nama</th>
-			<th>alamat</th>
-			<th>no_telepon</th>
-			
-		</tr>
-		@foreach($petugas as $p)
-		<tr>
-			<td>{{ $p->id }}</td>
-			<td>{{ $p->nama }}</td>
-			<td>{{ $p->alamat }}</td>
-			<td>{{ $p->no_telepon }}</td>
-			<td>
-				<a href="/petugas/edit/{{ $p->petugas_id }}">Edit</a>
-				|
-				<a href="/petugas/hapus/{{ $p->petugas_id }}">Hapus</a>
-			</td>
-		</tr>
-		@endforeach($petugas as $p)
-<tr>
-	<td>{{ $p->id }}</td>
-	<td>{{ $p->nama}}</td>
-	<td>{{ $p->alamat }}</td>
-	<td>{{ $p->no_telepon }}</td>
-	<td>
-		<a href="/petugas/edit/{{ $p->petugas_id }}">Edit</a>
-		|
-		<a href="/petugas/hapus/{{ $p->petugas_id }}">Hapus</a>
-	</td>
-</tr>
-@endforeach
+    <style>
+            html, body {
+                background-color: #D8BFD8;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
 
-</table>
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+</head>
+<body >
+    <center>
+        <h2>Daftar Petugas</h2>
+    </center>
+	
+   
+    
+        <div class="content">
+            <!-- Remove This Before You Start -->
+            
+            <hr>
+            <form action="{{ route('kontak.store') }}" method="post">
+             
+                <div class="form-group">
+                    <label for="nama">Nama:</label>
+                    <input type="text" class="form-control" id="usr" name="nama">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="nohp">No Hp:</label>
+                    <input type="text" class="form-control" id="nohp" name="nohp">
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat:</label>
+                    <textarea class="form-control" id="alamat" name="alamat"></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-md btn-primary">Submit</button>
+                    <button type="reset" class="btn btn-md btn-danger">Cancel</button>
+                </div>
+              
  
- 
+        <a href="/petugas/tambah"> + Tambah Petugas Baru</a>
+            </form>
+        </div>
+        <!-- /.content -->
+   
+        
+</body>
+</html>
+
 </body>
 </html>
