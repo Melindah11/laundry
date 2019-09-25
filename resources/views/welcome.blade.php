fa13b94455a5d2484b63938494c18b582ddbd980
Your Name <you@example.com>
2019-09-25 10:16:35 +0700

    apaan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laundry
-                    APAAN SEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEH
+                    APAAN SEEH
                 </div>
 
                 <div class="links">
