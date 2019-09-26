426b96cbcaab6485a373aaeafbe1764f661f7a31
Melindah11 <melindahmelmel@gmail.com>
2019-09-26 03:22:34 -0700

    pakett

--- app/Http/Controllers/PaketController.php
+++ app/Http/Controllers/PaketController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class PaketController extends Controller
+public function edit($id)
 {
     // mengambil data dari table paket
-    $paket = DB::table('paket')->get();
+    $paket = DB::table('paket')->where('id',$id)->get();
  
     // mengirim data paket ke view index
-    return view('PaketController',['paket' => $paket]);
+    return view(('edit',['paket' => $paket]);
 }
